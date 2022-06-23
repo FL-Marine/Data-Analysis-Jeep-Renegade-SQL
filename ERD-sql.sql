2e3c6068a96dfd71558ea7d99a75d7a6fa635cb2
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-06-23 17:22:25 -0400

    Create ERD-sql.sql

---
+++ ERD-sql.sql
@@ -0,0 +1,11 @@
+TABLE "jeep_data" {
+  "unique_visitors" INTEGER
+  "form_start" INTEGER
+  "form_completes" INTEGER
+  "channel" VARCHAR(300)
+  "form_name" VARCHAR(300)
+  "new_date" DATE
+  "browser" VARCHAR(300)
+  "country" VARCHAR(300)
+  "visitor_type" VARCHAR(300)
+  }
