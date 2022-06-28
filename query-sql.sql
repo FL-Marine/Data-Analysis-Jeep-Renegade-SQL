03dffdfb1dc4dbd2bd59d583a2099596c75c6e0c
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-06-28 16:08:12 -0400

    Update query-sql.sql

--- query-sql.sql
+++ query-sql.sql
@@ -1,4 +1,5 @@
-** How many rows?**
+**How many rows?**
+
 SELECT
 	COUNT(*) as row_count
 FROM jeep_renegade.jeep_data;
@@ -6,3 +7,43 @@ FROM jeep_renegade.jeep_data;
 | row_count |
 | --------- |
 | 23900     |
+
+**Showing Unique Values**
+
+    SELECT DISTINCT
+    	country
+    FROM jeep_renegade.jeep_data;
+
+| country |
+| ------- |
+| Unknown |
+| USA     |
+| Canada  |
+| India   |
+
+    SELECT DISTINCT
+    	channel
+    FROM jeep_renegade.jeep_data;
+
+| channel                                  |
+| ---------------------------------------- |
+| Other                                    |
+| Organic Search                           |
+| Direct Mail                              |
+| PPC                                      |
+| Auto Sites (Cars.com, Car & Drive, etc.) |
+| Build & Price Tool                       |
+
+
+    SELECT DISTINCT
+    	browser
+    FROM jeep_renegade.jeep_data;
+
+| browser           |
+| ----------------- |
+| Other             |
+| Safari            |
+| Firefox           |
+| Chrome            |
+| Internet Explorer |
+
