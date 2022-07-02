21557294889ffe07d1537a4a493efc0d17bb4cf0
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-07-02 13:35:59 -0400

    Update query-sql.sql

--- query-sql.sql
+++ query-sql.sql
@@ -94,3 +94,40 @@ ORDER BY frequency DESC;
 
 **Adding a Percentage Column**
 
+    SELECT
+    	country,
+        COUNT(*) AS frequency,
+        COUNT(*)::NUMERIC / SUM(COUNT(*)) OVER () AS percentage
+    FROM jeep_renegade.jeep_data
+    GROUP BY country
+    ORDER BY frequency DESC;
+
+| country | frequency | percentage             |
+| ------- | --------- | ---------------------- |
+| USA     | 12083     | 0.50556485355648535565 |
+| Unknown | 7336      | 0.30694560669456066946 |
+| Canada  | 2515      | 0.10523012552301255230 |
+| India   | 1966      | 0.08225941422594142259 |
+
+
+SELECT
+    	country,
+        COUNT(*) AS frequency,
+        ROUND(
+          100 * COUNT(*)::NUMERIC / SUM(COUNT(*)) OVER (), 2
+        ) AS percentage
+    FROM jeep_renegade.jeep_data
+    GROUP BY country
+    ORDER BY frequency DESC;
+
+| country | frequency | percentage |
+| ------- | --------- | ---------- |
+| USA     | 12083     | 50.56      |
+| Unknown | 7336      | 30.69      |
+| Canada  | 2515      | 10.52      |
+| India   | 1966      | 8.23       |
+
+/* Percentage decimal is a bit ugly. 
+Multiple percentage value by 100 and round it to 1 decimal place using ROUND function
+Note of the 100 * is used in the query below and notice the comma within the round brackets for the ROUND function.*/
+
